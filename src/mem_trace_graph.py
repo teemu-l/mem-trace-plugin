e6ce1b4f770e0ae1b87da7694586af30c7088c74
Teemu <14867206+teemu-l@users.noreply.github.com>
2020-04-16 12:51:18 +0300

    Update

--- src/mem_trace_graph.py
+++ src/mem_trace_graph.py
@@ -18,7 +18,6 @@ DEFAULT_MAX_DISTANCE = 30
 
 
 class PluginMemTrace(IPlugin):
-
     def init_gui(self):
         self.win = pg.GraphicsWindow(title="Memory trace graph plugin")
 
@@ -30,33 +29,33 @@ class PluginMemTrace(IPlugin):
         self.layout = self.win.addLayout(row=0, col=0)
         self.layout.addItem(self.proxy)
 
-        self.plot_item = self.win.addPlot(title='Memory trace graph', row=1, col=0)
-        self.plot_item.setLabel('left', "Memory address index")
-        self.plot_item.setLabel('bottom', "Trace row id")
+        self.plot_item = self.win.addPlot(title="Memory trace graph", row=1, col=0)
+        self.plot_item.setLabel("left", "Memory address index")
+        self.plot_item.setLabel("bottom", "Trace row id")
 
-        self.scatter_plot_reads = pg.ScatterPlotItem(symbol='o')
+        self.scatter_plot_reads = pg.ScatterPlotItem(symbol="o")
         self.scatter_plot_reads.setBrush(None)
-        self.scatter_plot_reads.setPen('g')
+        self.scatter_plot_reads.setPen("g")
         self.scatter_plot_reads.sigClicked.connect(self.sig_clicked)
 
-        self.scatter_plot_writes = pg.ScatterPlotItem(symbol='o')
-        self.scatter_plot_writes.setBrush('r')
-        self.scatter_plot_writes.setPen('r')
+        self.scatter_plot_writes = pg.ScatterPlotItem(symbol="o")
+        self.scatter_plot_writes.setBrush("r")
+        self.scatter_plot_writes.setPen("r")
         self.scatter_plot_writes.sigClicked.connect(self.sig_clicked)
 
         self.plot_item.addItem(self.scatter_plot_writes)
         self.plot_item.addItem(self.scatter_plot_reads)
 
-        self.plot_item.showGrid(y=True, x=True, alpha=1.)
+        self.plot_item.showGrid(y=True, x=True, alpha=1.0)
 
     def sig_clicked(self, points):
         spot_item = points.ptsClicked[0]
         row_id = int(spot_item.pos()[0])
         if len(points.ptsClicked) > 1:
-            print(f"Points clicked, rows: {[int(x.pos()[0]) for x in points.ptsClicked]}")
-        else:
-            print(f"Point clicked, row: {row_id}")
-        self.api.go_to_trace_row(row_id)
+            print(
+                f"Multiple points clicked, rows: {[int(x.pos()[0]) for x in points.ptsClicked]}"
+            )
+        self.api.go_to_row_in_full_trace(row_id)
 
     def mem_combo_box_changed(self, index):
         self.plot_item.clear()
@@ -72,13 +71,12 @@ class PluginMemTrace(IPlugin):
         self.api = api
 
         max_distance = api.get_string_from_user(
-            "Max distance", 
-            f"Give a max distance between addresses, leave empty for default ({DEFAULT_MAX_DISTANCE})"
+            "Max distance",
+            f"Give a max distance between addresses, leave empty for default ({DEFAULT_MAX_DISTANCE})",
         )
         try:
             max_distance = int(max_distance)
         except ValueError:
-            print("using default")
             max_distance = DEFAULT_MAX_DISTANCE
 
         self.init_gui()
@@ -92,17 +90,17 @@ class PluginMemTrace(IPlugin):
 
         # get all memory accesses from trace
         for t in trace:
-            if t['mem']:
-                if t['id'] > row_id_max:
-                    row_id_max = t['id']
-                if t['id'] < row_id_min:
-                    row_id_min = t['id']
-                for mem in t['mem']:
-                    if mem['access'] == 'READ':
-                        data_reads.append({"x": t['id'], "y": mem['addr']})
-                    elif mem['access'] == 'WRITE':
-                        data_writes.append({"x": t['id'], "y": mem['addr']})
-                    addresses.add(mem['addr'])
+            if t["mem"]:
+                if t["id"] > row_id_max:
+                    row_id_max = t["id"]
+                if t["id"] < row_id_min:
+                    row_id_min = t["id"]
+                for mem in t["mem"]:
+                    if mem["access"] == "READ":
+                        data_reads.append({"x": t["id"], "y": mem["addr"]})
+                    elif mem["access"] == "WRITE":
+                        data_writes.append({"x": t["id"], "y": mem["addr"]})
+                    addresses.add(mem["addr"])
 
         # convert a set to sorted list
         addresses = sorted(addresses)
@@ -113,16 +111,16 @@ class PluginMemTrace(IPlugin):
         i = 0
         for (i, addr) in enumerate(addresses):
             if i > 0:
-                if abs(addr - addresses[i-1]) > max_distance:
+                if abs(addr - addresses[i - 1]) > max_distance:
                     new_index += max_distance
             addr_indexes[addr] = new_index + i
         addr_max = new_index + i
 
         # replace addresses with indexes
         for d in data_reads:
-            d['y'] = addr_indexes[d['y']]
+            d["y"] = addr_indexes[d["y"]]
         for d in data_writes:
-            d['y'] = addr_indexes[d['y']]
+            d["y"] = addr_indexes[d["y"]]
 
         self.plot_item.setXRange(row_id_min, row_id_max, padding=0)
         self.plot_item.setYRange(1, addr_max, padding=0)
